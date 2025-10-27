cebdf1b7f2e2e8e2277f24ad726a1381af1be6e2
Mikołaj <zhiltsov2281337@gmail.com>
2025-10-27 14:30:46 +0300

    second try

---
+++ src/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+import "./Gallery.css";
+
+function Gallery() {
+  return (
+    <section className="gallery section">
+      <img className="gallery__image" alt="1" src="./img/stena.jpg" />
+      <img className="gallery__image" alt="2" src="./img/army.jpg" />
+      <img className="gallery__image" alt="3" src="./img/harbin.jpg" />
+      <img className="gallery__image" alt="4" src="./img/most.jpg" />
+    </section>
+  );
+}
+
+export default Gallery;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // разрешаем внешние подключения
+    port: 5173,
+    open: true, // автоматически открывать браузер
+  },
+  root: ".", // явно указываем корневую директорию
+  base: "./", // для Netlify
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
+});
